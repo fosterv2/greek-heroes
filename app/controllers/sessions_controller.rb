2b5f5f5a29528836030186d71db4e17b6d4dbe21
github username <github email address>
2020-04-14 15:11:07 -0700

    fixed some login issues

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,8 +13,8 @@ class SessionsController < ApplicationController
     end
 
     def create
-        @user = User.find_by(name: params[:user][:username])
-        @user = user.try(:authenticate, params[:user][:password])
+        @user = User.find_by(username: params[:sessions][:username])
+        @user = @user.try(:authenticate, params[:sessions][:password])
         if @user
             session[:user_id] = @user.id
             redirect_to '/'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
             session[:user_id] = @user.id
             redirect_to '/'
         else
+            flash[:messages] = "Error!"
             redirect_to new_user_path
         end
     end
